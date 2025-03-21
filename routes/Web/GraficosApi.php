b06f936f2d99a12e058b1deacc0b98295def6297
MiguelGuevaraC <mguevaracaj@unprg.edu.pe>
2025-03-21 15:28:54 -0500

    feat:apis para dashboard liberados

---
+++ routes/Web/GraficosApi.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\GraficosApiController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('grafico-01', [GraficosApiController::class, 'avanceProyecto']);
+Route::get('grafico-03', [GraficosApiController::class, 'donacionesPorTipo']);
+Route::get('grafico-02', [GraficosApiController::class, 'estadoFinanciero']);
+Route::get('dashboard', [GraficosApiController::class, 'dashboard_resumen']);
+Route::get('dashboard-by-project/{id}', [GraficosApiController::class, 'dashboard_by_project']);
